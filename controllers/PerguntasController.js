5fea67dc281b11ffdb2a86ca97040708a02ea5b9
renanlimabl <renanlimabl@gmail.com>
2020-12-29 18:26:53 -0300

    Adiciona mais metodos controllers e refatora rotas

--- controllers/PerguntasController.js
+++ controllers/PerguntasController.js
@@ -1,5 +1,8 @@
+const client = require('../config/postgres');
+
 const PerguntasController = {
-  create: (table_name, ...columns) => {
+  // utils
+  createQuery: (table_name, ...columns) => {
     // columns
     const textValues = [...columns].join(',');
 
@@ -12,7 +15,33 @@ const PerguntasController = {
 
 
     return `INSERT INTO ${table_name}(${textValues}) VALUES(${numbersOfValues}) RETURNING *`
-  }
+  },
+  create: async (request, response) => {
+    // const queryText = 'INSERT INTO perguntas_table(titulo, descricao) VALUES($1, $2) RETURNING *';
+    const queryText = PerguntasController.createQuery('perguntas_table', 'titulo', 'descricao')
+
+    const values = ['Agora foi!!', 'Postgres e Nodejs é massa :D'];
+    try {
+      const data = await client.query(queryText, values)
+      console.log(data.rows)
+      return response.status(200).json({ response: data.rows })
+    } catch (e) {
+      console.log(e.stack)
+    }
+  },
+  // utils
+  queryGet: (table_name) => {
+    return `SELECT * FROM ${table_name}`
+  },
+  all: async (request, response) => {
+    const queryText = PerguntasController.queryGet('perguntas_table');
+    try {
+      const data = await client.query(queryText)
+      return response.status(200).json({ response: data.rows })
+    } catch (e) {
+      console.log(e)
+    }
+  },
 }
 
 module.exports = PerguntasController;
--- index.js
+++ index.js
@@ -1,37 +1,13 @@
 const express = require('express');
-const client = require('./config/postgres');
-
 const app = express();
 
 app.use(express.json())
 
 const PerguntasController = require('./controllers/PerguntasController');
 
-app.get('/', async (request, response) => {
-  const queryText = 'SELECT * FROM perguntas_table';
+app.get('/', PerguntasController.all);
 
-  try {
-    const data = await client.query(queryText)
-    return response.status(200).json({ response: data.rows })
-  } catch (e) {
-    console.log(e)
-  }
-
-
-});
-
-app.post('/save', async (request, response) => {
-  // const queryText = 'INSERT INTO perguntas_table(titulo, descricao) VALUES($1, $2) RETURNING *';
-  const queryText = PerguntasController.create('perguntas_table', 'titulo', 'descricao')
-  const values = ['Agora foi!!', 'Postgres e Nodejs é massa :D'];
-  try {
-    const data = await client.query(queryText, values)
-    console.log(data.rows)
-    return response.status(200).json({ response: data.rows })
-  } catch (e) {
-    console.log(e.stack)
-  }
-});
+app.post('/save', PerguntasController.create);
 
 app.listen(3000, () => {
   console.log(`Servidor rodando na porta 3000`)
